2d9763a4de0b36e1034bc655a7c2107609230a8f
Eric <espinoza.eric@gmail.com>
2022-02-05 01:28:14 -0300

    promesa ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ fetch("https://api.github.com/users")
 const request = async (url) => {
     const fetch_url = await fetch(url)
     const response = fetch_url.json()
-    return console.log(response)
+    return response
 }
 // request("https://api.github.com/users")
 
@@ -18,28 +18,31 @@ const getUser = async (usuario) => {
     const url = `https://api.github.com/users/${usuario}`
     return await request(url)
 }
-getUser("erc83")
+//getUser("erc83")
 //getUser()
 
 const getRepositorios = async (usuario, pag, repos_pag) => {
     const url = `https://api.github.com/users/${usuario}/repos?page=${pag}per_page=${repos_pag}`
     return await request(url)
 }
-getRepositorios("erc83",1,1 )
-
-
+//getRepositorios("erc83",1,1 )
 
 let formulario = document.querySelector("form")
 
 let buscador = (event) => {
     event.preventDefault()
-    let usuario = document.getElementById("nombre").value
-    let pag = parseInt(document.getElementById("pagina").value)
-    let repos_pag = parseInt(document.getElementById("repoPagina").value)
+    let nombre = document.getElementById("nombre").value
+    let pagina = parseInt(document.getElementById("pagina").value)
+    let reposPagina = parseInt(document.getElementById("repoPagina").value)
 
     let resultados = document.getElementById("resultados")
 
-    if (usuario, pag, repos_pag){
+    if (nombre && pagina && reposPagina){
+        Promise.all([getUser(nombre), getRepositorios(nombre, pagina, reposPagina)])
+        .then(resp => {
+            console.log(resp)
+        })
+
 
     }else{
         alert("No se puede consultar")
