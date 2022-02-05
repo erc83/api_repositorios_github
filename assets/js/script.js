89d2f3cbe67e843422c0aa5364145f44a5f89d01
Eric <espinoza.eric@gmail.com>
2022-02-05 00:15:03 -0300

    consultado las url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
+// baseUrl = "https://api.github.com/users"
+// url User = https://api.github.com/users/erc83/repos?page=1per_page=1
+
+
+fetch("https://api.github.com/users")
+.then(response => response.json())
+.then(data => console.log(data))
+
+
+const request = async (url) => {
+    const fetch_url = await fetch(url)
+    const response = fetch_url.json()
+    return console.log(response)
+}
+// request("https://api.github.com/users")
+
+const getUser = async (usuario) => {
+    const url = `https://api.github.com/users/${usuario}`
+    return await request(url)
+}
+//getUser("erc83")
+getUser()
+
+const getRepositorios = async () => {
+    const url = `https://api.github.com/users/erc83/repos?page=1per_page=1`
+    return await request(url)
+}
+getRepositorios()
+
+
+
 
 
 
